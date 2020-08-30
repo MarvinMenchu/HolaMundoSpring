c634275ad1705291ed869c813bcc3b4b1caf7975
Marvin Manuel Menchú Menchú <marvin.menchu@icloud.com>
2020-08-29 22:00:41 -0600

    Nueva linea
    Conexión SSH

--- src/main/java/io/fenix/demo/HolaMundoSpringApplication.java
+++ src/main/java/io/fenix/demo/HolaMundoSpringApplication.java
@@ -10,6 +10,7 @@ public class HolaMundoSpringApplication {
 		SpringApplication.run(HolaMundoSpringApplication.class, args);
 		System.out.println("Hola Mundo con Spring boot");
 		System.out.println("Nueva Linea modificada desde GitHub");
+		System.out.println("Nueva Linea modificada desde GitHub con conexión SSH");
 	}
 
 }

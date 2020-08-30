6ded1bae882d0240fa7dce602d05014553d060a6
Marvin Menchu <marvin.menchu@icloud.com>
2020-08-29 19:11:53 -0600

    Se crea el proyecto Hola Mundo con Spring boot

---
+++ src/main/java/io/fenix/demo/HolaMundoSpringApplication.java
@@ -0,0 +1,14 @@
+package io.fenix.demo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class HolaMundoSpringApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(HolaMundoSpringApplication.class, args);
+		System.out.println("Hola Mundo con Spring boot");
+	}
+
+}
---
+++ src/test/java/io/fenix/demo/HolaMundoSpringApplicationTests.java
@@ -0,0 +1,13 @@
+package io.fenix.demo;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class HolaMundoSpringApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
